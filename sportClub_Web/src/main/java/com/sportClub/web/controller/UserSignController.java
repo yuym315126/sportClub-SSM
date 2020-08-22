c688a4ee92bc6df91b7d7aa1db1f9a112105bb06
yuym315126 <1437903686@qq.com>
2020-08-22 20:45:46 +0800

    userSign

--- sportClub_Web/src/main/java/com/sportClub/web/controller/UserSignController.java
+++ sportClub_Web/src/main/java/com/sportClub/web/controller/UserSignController.java
@@ -5,11 +5,12 @@ import com.sportClub.common.vo.R;
 import com.sportClub.provider.service.UserSignService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 /**
@@ -18,16 +19,18 @@ import javax.servlet.http.HttpServletRequest;
  * @description:
  */
 @Api(tags = "用户签到")
+@Slf4j
 @RestController
 @RequestMapping("userSign/")
 public class UserSignController {
-    @Autowired
+    @Resource
     private UserSignService userSignService;
 
     @ApiOperation("查询用户签到信息")
     @GetMapping("api/findUserSign.do")
     public R findUserSign(HttpServletRequest request){
         String token = request.getHeader(SystemConstant.TOKEN_HEADER);
+        System.err.println("token："+token);
         return userSignService.findUserSignIn(token);
     }
 
