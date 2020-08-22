803866e63c6c1ef4d2b8a852e635762d8822ce30
ma00hao <machenhao1996@vip.qq.com>
2020-08-22 21:04:37 +0800

    8.22登录

--- sportClub_UserLoginProvider/src/main/java/com/sportClub/provider/service/impl/UserLoginServiceImpl.java
+++ sportClub_UserLoginProvider/src/main/java/com/sportClub/provider/service/impl/UserLoginServiceImpl.java
@@ -5,6 +5,7 @@ import com.sportClub.common.config.RedisKeyConfig;
 import com.sportClub.common.dto.user.UserDto;
 import com.sportClub.common.dto.user.UserLoginDto;
 import com.sportClub.common.until.TokenUtil;
+import com.sportClub.common.util.JwtUtil;
 import com.sportClub.common.vo.R;
 import com.sportClub.pojo.User;
 import com.sportClub.provider.config.JedisCore;
@@ -20,7 +21,12 @@ public class UserLoginServiceImpl implements UserLoginService {
     @Autowired
     private JedisCore jedisCore;
 
-    //登录
+    /**
+     * 登录
+     *
+     * @param userLoginDto 用户手机号和密码
+     * @return
+     */
     public R Login(UserLoginDto userLoginDto) {
         if (jedisCore.checkKey(RedisKeyConfig.PHONE_TOKEN + userLoginDto.getPhone())) {
             return R.fail("不好意思您已经登录");
@@ -48,15 +54,15 @@ public class UserLoginServiceImpl implements UserLoginService {
     /**
      * 检查手机号是否存在
      *
-     * @param phone
+     * @param phone 输入的手机号
      * @return
      */
     @Override
     public R checkPhone(String phone) {
-        User user=userLoginDao.selectPhone(phone);
-        if(user!=null){
+        User user = userLoginDao.selectPhone(phone);
+        if (user != null) {
             return R.fail("手机号已经存在");
-        }else {
+        } else {
             return R.ok();
         }
     }
@@ -64,11 +70,21 @@ public class UserLoginServiceImpl implements UserLoginService {
     /**
      * 注册
      *
-     * @param userDto
+     * @param userDto 注册用户信息(手机号，密码，昵称)
      * @return
      */
     @Override
     public R register(UserDto userDto) {
-        return null;
+        if (checkPhone(userDto.getUserPhone()).getCode() == 200) {
+            userDto.setUserPwd(JwtUtil.createJWT(userDto.getUserPwd()));
+            User user = new User();
+            user.setUserNickname(userDto.getUserNickname());
+            user.setUserPhone(userDto.getUserPhone());
+            user.setUserPwd(userDto.getUserPwd());
+            userLoginDao.insert(user);
+            return R.ok("注册成功");
+        } else {
+            return R.fail("您的手机号已被注册");
+        }
     }
 }
